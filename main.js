41ce17d51f2ef00cfba86a084982287bdd75e25d
Aranog-blip <123972330+Aranog-blip@users.noreplay.github.com>
2023-04-07 00:41:32 +0200

    made buttons of calculator work

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
-let UserInput1S = prompt("Enter the first number");
-console.log (UserInput1S);
-console.log(typeof(UserInput1S));
-let operate = prompt("What funtion to use? (add, sub, times, divide)")
-console.log (operate);
-let UserInput2S = prompt ("Enter the second number");
-console.log (UserInput2S);
-let UserInput1 = Number(UserInput1S);
-let UserInput2 = Number(UserInput2S);
-console.log(typeof(UserInput1));
+
+function One(){
+    document.getElementById('display').innerHTML += 1 ;
+  };
+  function Two(){
+    document.getElementById("display").innerHTML += 2;
+  };
+  function Three (){
+    document.getElementById("display").innerHTML += 3;
+  };
+  function Four(){
+    document.getElementById("display").innerHTML += 4;
+  };
+  function Five(){
+    document.getElementById("display").innerHTML += 5;
+  };
+  function Six(){
+    document.getElementById("display").innerHTML += 6;
+  };
+  function Seven(){
+    document.getElementById("display").innerHTML += 7;
+  };
+  function Eight(){
+    document.getElementById("display").innerHTML +=8;
+  };
+  function Nine(){
+    document.getElementById("display").innerHTML += 9;
+  };
+  function Zero(){
+    document.getElementById("display").innerHTML += 0;
+  };
+
+document.getElementById("one").addEventListener("click", One);
+document.getElementById("two").addEventListener("click", Two);
+document.getElementById("three").addEventListener("click", Three);
+document.getElementById("four").addEventListener("click", Four);
+document.getElementById("five").addEventListener("click", Five);
+document.getElementById("six").addEventListener("click", Six);
+document.getElementById("seven").addEventListener("click", Seven);
+document.getElementById("eight").addEventListener("click", Eight);
+document.getElementById("nine").addEventListener("click", Nine);
+document.getElementById("zero").addEventListener("click", Zero);
 
 function add () {
     let x = UserInput1 + UserInput2;
@@ -29,15 +61,18 @@ function divide () {
     alert (x);
 
 }
-if (operate == "/"){
-    divide ();
-}
-else if (operate == "*"){
-    times();
-}
-else if (operate == "+"){
-    add();
-}
-else if (operate == "-"){
-    sub();
-}
+
+
+
+//if (operate == "/"){
+  //  divide ();
+//}
+//else if (operate == "*"){
+//    times();
+//}
+//else if (operate == "+"){
+  //  add();
+//}
+//else if (operate == "-"){
+  //  sub();
+//}
